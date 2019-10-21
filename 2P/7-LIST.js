e3d550dca0b9ee785d75a909b909fb919f168bcc
CUBOFIG <50375089+CUBOFIG@users.noreply.github.com>
2019-10-20 22:14:45 -0500

    Add files via upload

--- 2P/7-LIST.js
+++ 2P/7-LIST.js
@@ -67,18 +67,22 @@ class lista{
         return (this.head.data + ", " + this.Stringer(this.head.next));
     }
 }
-(function Uselista(){
+(function Usarlistas(){
+    
     let nodo1 = new nodo("nodo1");
     let nodo2 = new nodo("nodo2");
     let nodo3 = new nodo("nodo3");
     let nodo4 = new nodo("nodo4");
     let nodo5 = new nodo("nodo5");
-    let nodo6 = new nodo("nodo6");
-    let listaH = new lista;
-    listaH.insertHead(nodo5)
-    listaH.insertHead(nodo4)
-    listaH.insertHead(nodo3)
-    listaH.insertHead(nodo2)
-    listaH.insertHead(nodo1)
-    listaH.insertTail(nodo6)   
+    
+    let listaHeri = new lista;
+    listaHeri .insertHead(nodo5)
+    listaHeri .insertHead(nodo4)
+    listaHeri .insertHead(nodo3)
+    listaHeri .insertHead(nodo2)
+    listaHeri .insertHead(nodo1)
+  
 })()
+
+ 
+
